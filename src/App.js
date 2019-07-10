4dea4dbeaba3aa8ee2e475fa952fdf46ec3d2df0
Dmitry Kartsev <dkartsev@redhat.com>
2019-07-10 10:57:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './App.css';
+
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {joke: '', translated: ''};
+        this.onJokeClick();
+    }
+
+    getSelectedText = () => {
+        let selection = window.getSelection().toString();
+        if (selection.length > 0) {
+            console.log(selection);
+            fetch('http://localhost:8080/v1/translate/' + selection)
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    this.setState({
+                        translated: data.data
+                    });
+                });
+        }
+
+    };
+
+    onJokeClick = () => {
+        this.setState({
+            translated: ''
+        });
+        fetch('http://localhost:8080/v1/joke')
+            .then((response) => {
+                return response.json();
+            })
+            .then((jokeObj) => {
+                this.setState({
+                    joke: jokeObj.data
+                });
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="area">
+                    <ul className="circles">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div
+                    className="context"
+                    style={{textAlign: "center"}}>
+                    <h1 onMouseUp={this.getSelectedText}>
+                        {this.state.joke}
+                    </h1>
+                    <a onClick={this.onJokeClick} className="btn cta">Next</a>
+                    <h1>
+                        {this.state.translated}
+                    </h1>
+                </div>
+            </div>
+        )
+    };
+}
+
+
